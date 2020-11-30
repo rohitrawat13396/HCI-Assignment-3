b20be65198b68fd7844e6ca69999d59e88c80927
Rohit Rawat <rohitrawat13396@gmail.com>
2020-11-30 18:35:15 -0500

    Within Subjects
    -ANOVA and t-test added fro within subjects
    -reformed the output

--- q2.py
+++ q2.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from scipy.stats import ttest_ind
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
+import numpy as np
+from scipy.stats import ttest_rel
+from statsmodels.stats.anova import AnovaRM
 from collections import defaultdict
+import warnings
+warnings.filterwarnings("ignore")
 
-data = pd.read_csv("dataset1.csv")
+data = pd.read_csv("dataset2.csv")
 
 # ANOVA
-#Using OLS and anova_lm
-model = ols('time ~ C(menu)', data = data).fit()
-anova_table = sm.stats.anova_lm(model, typ=2)
-print("\nANOVA Between Subjects:\n", anova_table)
+F = AnovaRM(data, depvar= "time",subject = "user", within= ["menu"]).fit()
+print("\nANOVA Within Subjects:\n", F)
 
 # Pairwise t-test
 df = dict(list(data.groupby(["menu"])))
@@ -20,7 +20,7 @@ rel_dic = defaultdict(list)
 
 for m1 in df.keys():
     for m2 in df.keys():
-        stats, p = ttest_ind(df[m1].time,df[m2].time)
+        stats, p = ttest_rel(df[m1].time,df[m2].time)
         rel_dic[m1].append(p)
         statistics.append(stats)
         pvalue.append(p)
@@ -28,5 +28,5 @@ for m1 in df.keys():
 df_new = pd.DataFrame(rel_dic)
 df_new["menu"] = list(df_new.columns)
 df_new = df_new.set_index(keys = "menu")
-print("\nt-test Between Subjects:\n")
+print("\nt-test Within Subjects:\n")
 print(df_new)
